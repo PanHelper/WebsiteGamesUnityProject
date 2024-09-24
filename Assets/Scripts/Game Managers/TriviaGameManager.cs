ffeea59fe1bd8ef4f928a955d90c2a3f1fdacb74
Swimtime1 <aiden.crossfield@gmail.com>
2024-09-24 17:16:45 -0400

    Trivia Screen Swap
    Code has been implemented that will swap which screen is active in the Trivia Game

--- Assets/Scripts/Game Managers/TriviaGameManager.cs
+++ Assets/Scripts/Game Managers/TriviaGameManager.cs
@@ -1,18 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+/* using UnityEngine.InputSystem; */
 
 public class TriviaGameManager : MonoBehaviour
 {
+    #region Variables
+    
+    // GameObject Variables
+    [SerializeField] private GameObject startScreen, questionScreen, scoreScreen;
+
+    // TextMeshProUGUI Variables
+
+    // Boolean Variables
+    public static bool gameActive, gameStarted;
+
+    // Integer Variables
+
+    // BoxCollider2D Variables
+
+    // SpriteRenderer Variables
+
+    // Sprite Variables
+
+    // Image Variables
+
+    // Script Variables
+
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameActive = false;
+        gameStarted = false;
+        OpenStart();
     }
 
-    // Update is called once per frame
-    void Update()
+    #region Menu Operations
+    
+    // Deactivates every menu
+    void CloseMenus()
     {
-        
+        startScreen.SetActive(false);
+        questionScreen.SetActive(false);
+        scoreScreen.SetActive(false);
     }
+
+    // Opens the Start Screen
+    public void OpenStart()
+    {
+        gameActive = false;
+        gameStarted = false;
+        CloseMenus();
+        startScreen.SetActive(true);
+    }
+
+    // Opens the Question Screen
+    public void OpenQuestionScreen()
+    {
+        gameActive = false;
+        CloseMenus();
+        questionScreen.SetActive(true);
+        gameActive = true;
+        gameStarted = true;
+    }
+
+    // Opens the Score Screen
+    public void OpenScoreScreen()
+    {
+        gameActive = false;
+        CloseMenus();
+        scoreScreen.SetActive(true);
+    }
+
+    #endregion
 }
