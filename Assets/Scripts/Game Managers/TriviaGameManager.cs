d927ddd4554f8ae4e2acf4c7728e2998562a4b09
Swimtime1 <aiden.crossfield@gmail.com>
2024-09-27 17:33:31 -0400

    Update TriviaGameManager.cs
    Created mechanics to allow the player's answer to a question to affect the score, and remove the question answered from the list of options for the next question

--- Assets/Scripts/Game Managers/TriviaGameManager.cs
+++ Assets/Scripts/Game Managers/TriviaGameManager.cs
@@ -18,16 +18,13 @@ public class TriviaGameManager : MonoBehaviour
 
     // Boolean Variables
     public static bool gameActive, gameStarted;
+    [SerializeField] private List<bool> answers;
+
+    // String Variables
+    [SerializeField] private List<string> questions;
 
     // Integer Variables
-
-    // BoxCollider2D Variables
-
-    // SpriteRenderer Variables
-
-    // Sprite Variables
-
-    // Image Variables
+    private int currQuest, questsDone, score;
 
     // Script Variables
     private InputActions input;
@@ -45,6 +42,10 @@ public class TriviaGameManager : MonoBehaviour
     {
         gameActive = false;
         gameStarted = false;
+
+        questsDone = 0;
+        score = 0;
+
         OpenStart();
     }
 
@@ -108,6 +109,38 @@ public class TriviaGameManager : MonoBehaviour
     {
         // only opens the level selection menu if the start menu is active
         if(startScreen.activeSelf) { OpenQuestionScreen(); }
+    }
+
+    #endregion
+
+    #region Questions
+
+    // Determines whether the player got the question correct, and updates the score
+    public void Answer(bool ans)
+    {
+        // Checks for correct answer
+        if(ans == answers[currQuest])
+        {
+            score++;
+            // show the player they were right
+        }
+        else
+        {
+            // show the player they were wrong
+        }
+
+        questions.RemoveAt(currQuest);
+        answers.RemoveAt(currQuest);
+        questsDone++;
+
+        // Either moves to the next question or ends the game
+        if(questsDone < 5) {}
+        else 
+        {
+            // update score screen
+
+            OpenScoreScreen();
+        }
     }
 
     #endregion
